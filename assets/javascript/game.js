22edfec4fe671b59f7a60fb37c8327d50b42c474
unknown <mjtedder@gmail.com>
2018-05-05 21:36:33 -0400

    added html ids to select in js

---
+++ assets/javascript/game.js
@@ -0,0 +1,14 @@
+//Use key events to listen for letters that your players will type.
+
+//Display 'Press any key to get started on page'
+
+//Display 'Wins'
+
+// Display word as _ _ _ _ _ _ when game starts
+  //As user guesses letters correctly, reveal them: y o s _ _.
+
+// Display 'Number of Guesses Remaining:'
+
+  // 'Letters Already Guessed'
+
+// After game is over, restart game function should choose another word and clear other divs
